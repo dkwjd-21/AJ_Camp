8cc3db457e28e279c786a6d1803b51f2430cf5e5
dkwjd-21 <137870180+dkwjd-21@users.noreply.github.com>
2025-05-27 01:48:52 +0900

    [250526] 백준스터디 문풀

--- 2_java/Java00_Study/src/com/test01/Main.java
+++ 2_java/Java00_Study/src/com/test01/Main.java
@@ -6,37 +6,39 @@ public class Main {
 	public static void main(String[] args) throws IOException  {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
-		// 30명의 번호 명단
-		int[] list = new int[30];
-		for(int i=0; i<list.length; i++) {
-			list[i] = i+1;
-		}
+		// 색종이의 수
+		int n = Integer.parseInt(br.readLine());
 		
-		// 28줄의 입력
-		int[] input = new int[28];
-		for(int i=0; i<input.length; i++) {
-			input[i] = Integer.parseInt(br.readLine());
-		}
+		// 100*100 도화지
+		boolean[][] paper = new boolean[100][100];
 		
-		// 30명의 번호 명단과 28줄의 입력 비교 
-		int min = list[29];
-		int max = list[0];
-		for(int i=0; i<input.length; i++) {
-			for(int j=0; j<list.length; j++) {
-				if(input[i] != list[j]) {
-					if(input[i] > max) {
-						max = input[i];
-					}else if(input[i] < min) {
-						min = input[i];
-					}
-				}
-				else {
-					break;
+		// 색종이의 수만큼 반복문으로 색종이의 왼쪽 아래 점 좌표 입력받음
+		for(int i=0; i<n; i++) {
+			String[] input = br.readLine().split(" ");
+			int x = Integer.parseInt(input[0]);
+			int y = Integer.parseInt(input[1]);
+			
+			// [x][y]~[x+10][y+10] 까지 true로 바꾸기 
+			// x와 y는 도화지의 왼쪽&아랫쪽 변 사이의 거리이기 때문에 0부터 시작함
+			// = 배열의 인덱스와 일치함 
+			for(int j=x; j<x+10; j++) {
+				for(int k=y; k<y+10; k++) {
+					paper[j][k] = true;
 				}
 			}
 		}
 		
-		System.out.println(min);
-		System.out.println(max);
+		// true 개수 = 넓이
+		int cnt = 0;
+		for(int i=0; i<paper.length; i++) {
+			for(int j=0; j<paper[i].length; j++) {
+				if(paper[i][j] == true) {
+					cnt++;
+				}
+			}
+		}
+		
+		// 넓이 출력
+		System.out.println(cnt);
 	}
 }
