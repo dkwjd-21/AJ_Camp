48c5ce03dc7efce23f3dbe15a6ee3a60241ef086
dkwjd-21 <137870180+dkwjd-21@users.noreply.github.com>
2025-06-09 00:18:43 +0900

    [250608] 백준문풀

--- 2_java/Java00_Study/src/com/baekjoon/Main.java
+++ 2_java/Java00_Study/src/com/baekjoon/Main.java
@@ -3,14 +3,30 @@ package com.baekjoon;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.HashMap;
 
 public class Main {
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
-		HashMap<String, Integer> score = new HashMap<>();
+		/*	규칙
+		 * 첫줄의 정사각형의 개수는 2^n
+		 * 첫줄의 점의 개수는 2^n+1
+		 * 전체 점의 개수는 2^n+1의 제곱
+		 * */
 		
+		// N이 주어진다
+		int N = Integer.parseInt(br.readLine());
+		System.out.println(countDot(N));
+		
+		br.close();
+	}
+	
+	public static int countDot(int N) {
+		double cnt = 0.0;
+		
+		cnt = Math.pow(Math.pow(2, N)+1, 2);
+		
+		return (int)cnt;
 	}
 }
 
