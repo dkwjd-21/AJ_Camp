7f6e127c74854efab16ad3bad1b30c8a8362f89c
dkwjd-21 <137870180+dkwjd-21@users.noreply.github.com>
2025-06-16 02:47:26 +0900

    스터디준비 ..

--- 2_java/Java00_Study/src/com/baekjoon/Main.java
+++ 2_java/Java00_Study/src/com/baekjoon/Main.java
@@ -3,28 +3,30 @@ package com.baekjoon;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
 
 public class Main {
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
-		// 정수 n 입력
-		int n = Integer.parseInt(br.readLine());
+		// 정수 N 
+		int N = Integer.parseInt(br.readLine());
 		
-		while(n>0) {
-			int sub = 0;
-			// for문을 돌며 1, 11, 111, 1111 ... 을 나눈 값이 0이면 다음 케이스 진행
-			for(int i=0; i<=n; i++) {
-				sub += (int)Math.pow(10, i);
-				
-				if(sub%n == 0) {
-					System.out.println(i+1);
-					break;
+		// N을 소인수분해한 결과를 한줄에 하나씩 오름차순으로 출력 
+		// N이 1인 경우 아무것도 출력하지 않음
+		if(N != 1) {
+			for(int i=2; i<=N; i++) {
+				// i로 N을 계속 나눈다. 
+				while(N%i == 0) {
+					// i로 나눠떨어지면 i는 N의 약수이다. 
+					System.out.println(i);
+					// N의 값을 i로 나눈 몫으로 변경한다. 
+					N = N/i;
 				}
 			}
-			
-			n = Integer.parseInt(br.readLine());
-		}
+		}		
+		br.close();
 	}
 }
 
